1d2038c90f9a10a96c9772000eca5b8a321719d4
xpengStudy <xpengStudy@126.com>
2020-10-25 10:56:20 +0800

    初始化

---
+++ test2/src/main/java/com/xp/child/Test.java
@@ -0,0 +1,12 @@
+package com.xp.child;
+
+public class Test {
+
+    public void hello(){
+        System.out.println("hello  已经覆盖了哦！");
+    }
+
+    public String getHello(){
+        return "new hello";
+    }
+}
---
+++ test3/src/main/java/MainInPub.java
@@ -0,0 +1,9 @@
+import com.xp.child.Test;
+
+public class MainInPub {
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.hello();
+    }
+}
